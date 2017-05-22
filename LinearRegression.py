b4b6863e6cc6decb654af2e68d27d96249a8800c
Lars <lfessen@web.de>
2017-05-22 19:55:16 +0200

    moved gradient descent in method

--- LinearRegression.py
+++ LinearRegression.py
@@ -4,22 +4,22 @@ import numpy as np
 class LinearRegression(object):
     """
     Basic LinearRegression
-    
+
     Parameters
     ----------
     n_iter : int, default 1000
         number of iterations the gradient descent should process
-        
+
     eta : int, default 0.1
-        the learning rate. 
+        the learning rate.
         High learning rate: might overshoot the minimum
         Low learning rate: gradient descent could take too long (does not reach minimum within n_iter)
-        
+
     Attributes
     ----------
     weights : array, shape(n_features, )
         Coefficients for the hypothesis. The weights will be 'fitted' during the gradient descent
-    
+
     costs_per_iter : array, shape(n_iter, )
         Contains the sum of costs for every iteration
     """
@@ -33,33 +33,35 @@ class LinearRegression(object):
     def fit(self, X, y):
         """
         Fit LinearRegression with gradient descent
-        
+
         Parameters
         ----------
         X : numpy array, shape(m_samples, n_features)
             Training data
-            
+
         y : numpy array, shape (m_samples, )
             Target values
         """
 
         self.weights = np.zeros(1 + X.shape[1])
-        weights_tmp = np.zeros(1 + X.shape[1])
-        m_samples = len(y)
+        self.weights_tmp = np.zeros(1 + X.shape[1])
 
         for _ in range(self.n_iter):
-            # Weight 0 has to be calculated separately
-            weights_tmp[0] = self.weights[0] - self.eta / m_samples * self.sigma(X, y, np.ones(X.shape[0]))
+            self.gradient_descent(X, y)
 
-            # Update all weights with gradient descent (in weights_tmp)
-            for j in range(len(self.weights) - 1):
-                weights_tmp[j + 1] = self.weights[j + 1] - self.eta / m_samples * self.sigma(X, y, X[:, j])
+    def gradient_descent(self, X, y):
+        # Weight 0 has to be calculated separately
+        self.weights_tmp[0] = self.weights[0] - self.eta / len(y) * self.sigma(X, y, np.ones(X.shape[0]))
 
-            # Update all weights simultaneously
-            self.weights = weights_tmp
+        # Update all weights with gradient descent (in weights_tmp)
+        for j in range(len(self.weights) - 1):
+            self.weights_tmp[j + 1] = self.weights[j + 1] - self.eta / len(y) * self.sigma(X, y, X[:, j])
 
-            # Append current costs (not necessary, just to visualize gradient descent)
-            self.costs_per_iter.append(np.absolute(self.costs(X, y)).sum())
+        # Update all weights simultaneously
+        self.weights = self.weights_tmp
+
+        # Append current costs (not necessary, just to visualize gradient descent)
+        self.costs_per_iter.append(np.absolute(self.costs(X, y)).sum())
 
     def sigma(self, X, y, x_feature):
         """
@@ -73,12 +75,12 @@ class LinearRegression(object):
     def predict(self, X):
         """
         Predict the target value(s)
-        
+
         Parameters
         ----------
         X : numpy array, shape(m_samples, n_features)
             Data to use for prediction
-            
+
         Returns
         -------
         prediction : numpy array, shape(m_samples, )
