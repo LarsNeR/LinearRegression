4c09099ed79394f5df6cfe545f152364f25c5782
Lars <lfessen@web.de>
2017-05-30 21:42:07 +0200

    Using vectorized gradient descent instead of iterative gradient descent (for loop). Adding one column with ones to the feature matrix X for this (see comments).

--- LinearRegression.py
+++ LinearRegression.py
@@ -24,7 +24,7 @@ class LinearRegression(object):
         Contains the sum of costs for every iteration
     """
 
-    def __init__(self, n_iter=1000, eta=0.1):
+    def __init__(self, n_iter=100000, eta=0.01):
         self.n_iter = n_iter
         self.eta = eta
         self.weights = []
@@ -43,31 +43,15 @@ class LinearRegression(object):
             Target values
         """
 
-        self.weights = np.zeros(1 + X.shape[1])
-        self.weights_tmp = np.zeros(1 + X.shape[1])
+        self.weights = np.zeros(X.shape[1])
+        self.weights_tmp = np.zeros(X.shape[1])
 
         for _ in range(self.n_iter):
-            self.gradient_descent(X, y)
+            # Gradient descent
+            self.weights -= self.eta * np.dot(X.T, self.costs(X, y)) / len(y)
 
-    def gradient_descent(self, X, y):
-        # Weight 0 has to be calculated separately
-        self.weights_tmp[0] = self.weights[0] - self.eta / len(y) * self.sigma(X, y, np.ones(X.shape[0]))
-
-        # Update all weights with gradient descent (in weights_tmp)
-        for j in range(len(self.weights) - 1):
-            self.weights_tmp[j + 1] = self.weights[j + 1] - self.eta / len(y) * self.sigma(X, y, X[:, j])
-
-        # Update all weights simultaneously
-        self.weights = self.weights_tmp
-
-        # Append current costs (not necessary, just to visualize gradient descent)
-        self.costs_per_iter.append(np.absolute(self.costs(X, y)).sum())
-
-    def sigma(self, X, y, x_feature):
-        """
-        The sigma in gradient descent (derivative)
-        """
-        return (np.dot(self.costs(X, y), x_feature)).sum()
+            # Append current costs (not necessary, just for visualization purposes)
+            self.costs_per_iter.append(np.absolute(self.costs(X, y)).sum())
 
     def costs(self, X, y):
         return self.predict(X) - y
@@ -86,4 +70,4 @@ class LinearRegression(object):
         prediction : numpy array, shape(m_samples, )
             Prediction for every sample in X
         """
-        return np.dot(X, self.weights[1:]) + self.weights[0]
+        return np.dot(X, self.weights)
--- Main.py
+++ Main.py
@@ -3,26 +3,35 @@ from LinearRegression import LinearRegression
 import matplotlib.pyplot as plt
 from sklearn import datasets, preprocessing, model_selection
 
-# Load dataset (you can also load your own with pandas but sklearn offers a range of different datasets)
+# Load dataset (you can also load your own with pandas but sklearn offers
+# a range of different datasets)
 (X, y) = datasets.load_boston(return_X_y=True)
 
 # Preprocess it with sklearn (not necessary, but improves gradient descent)
 X = preprocessing.scale(X)
 
+# Add one column only with ones (for weight_0 , the weight 'without' feature)
+# Otherwise you have to calculate weight[0] and weight[1:] separately
+# If you want to use that check the history (before 30.05.2017)
+X = np.concatenate([np.ones((len(y), 1)), X], axis=1)
+
 # Divide dataset into train and test data
 X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, train_size=0.65)
 
-# Instantiate a new LinearRegression and call its fit-method with the train data
+# Instantiate a new LinearRegression and call its fit-method with the
+# train data
 lr = LinearRegression()
 lr.fit(X=X_train, y=y_train)
 
-# Predict the result with the test data and calculate the absolute costs between the prediction and the target values
+# Predict the result with the test data and calculate the absolute costs
+# between the prediction and the target values
 y_pred = lr.predict(X_test)
 costs = np.absolute(y_pred - y_test).sum()
 
 # Print interesting information
 print(costs)
 print(costs / len(X_test))
+print(lr.weights)
 
 # Plot costs per iteration
 plt.plot(lr.costs_per_iter, marker='o')
